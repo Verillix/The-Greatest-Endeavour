b8b9fdd3d7eb77bd7367f78804a42a182414c8e5
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-17 18:17:01 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-import os
-SECRET = os.environ['TOKEN']
-if SECRET == "BLEH":
-  print("yippeee")
+import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    #  Wait for next request from client
+    message = socket.recv()
+    print(f"Received request: {message}")
+
+    #  Do some 'work'
+    time.sleep(1)
+
+    #  Send reply back to client
+    socket.send(b"World")
