1a4d032b97d9ea974017635cb84c09216fe07295
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-17 17:46:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pypdf import PdfReader,PdfWriter
 from pyodide.http import open_url
 from dotenv import dotenv_values
 
-
+#HIIIII
 
 class StrToBytes:
     def __init__(self, fileobj):
@@ -51,3 +51,4 @@ async def downloaded(*args):
 
 
         display(error)
+
