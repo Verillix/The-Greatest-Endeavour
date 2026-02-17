93e70e8fdde49d95c8114ae771521d1c85852932
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-17 20:40:44 +0000

    Update apiGET.py

--- api/apiGET.py
+++ api/apiGET.py
@@ -3,13 +3,14 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        github_token = os.environ.get('GITHUB_TOKEN')
+        github_token = os.environ.get('API_KEY')
 
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(github_token.encode())
+        print(github_token)
     
     def do_OPTIONS(self):
         self.send_response(200)
