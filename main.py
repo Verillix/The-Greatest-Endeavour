d04e4813e7c98e49851969f4dcd83b609df5fea9
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-17 21:41:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -43,9 +43,10 @@ async def downloadTex():
     response = await fetch(url)
     try:
         with open('The Greatest Endeavour.tex', 'wb') as out_file:
-          out_file.write(response.raw())
+          out_file.write(response)
     except Exception as error:
         display(error)
+
 
 
 
