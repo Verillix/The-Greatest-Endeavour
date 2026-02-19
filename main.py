d0608efd052fe6a0b70d992c59fa5c65ed33784f
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-19 15:02:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def processTex(*args):
     
 async def push_file(content, filename):
     response = await fetch(
-        'https://the-greatest-endeavour.vercel.app/api/gitPush',
+        'https://the-greatest-endeavour.vercel.app/api/gitPUSH',
         method='POST',
         headers=to_js({'Content-Type': 'application/json'}),
         body=json.dumps({'filename': filename, 'content': content})
@@ -91,6 +91,7 @@ def readCurrent():
     reader = PdfReader(pdf)
     display(reader.pages[0].extract_text())
 '''
+
 
 
 
