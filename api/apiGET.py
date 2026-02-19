8ac3ccfbf36386bb0604c3dce88087fac7461364
Verillix <139095640+Verillix@users.noreply.github.com>
2026-02-19 14:28:37 +0000

    Update apiGET.py

--- api/apiGET.py
+++ api/apiGET.py
@@ -4,7 +4,7 @@ import base64
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        github_token = os.environ.get('API_KEY')
+        github_token = os.environ.get('GIT_TOKEN')
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
         self.send_header('Access-Control-Allow-Origin', '*')
