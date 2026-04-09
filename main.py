87d744c34865c57e0683920a9a290de8b78f56ec
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-09 21:20:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def download_file(data, filename):
         destination = Path.home() / "LaTeX"
         destination.mkdir(exist_ok=True, parents=True)
         fs = fsspec.filesystem("github", org="Verillix", repo="The-Greatest-Endeavour")
+        print(fs.ls)
         fs.get(fs.ls("https://github.com/Verillix/The-Greatest-Endeavour/tree/d5d0292a5e582efae1f160c6b6bb0d41f3bfdc1c/LaTeX/The%20Greatest%20Endeavour.tex"), destination.as_posix())
 '''
     #blob = Blob.new([data], {"type": "application/dir"})
