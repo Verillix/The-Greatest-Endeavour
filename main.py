135e7c4ae778555f4393e2f4fbf0a4b9c0eb7ba2
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-09 19:35:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def push_file(content, filename):
 
 @when('click', '#downloadTex')
 async def downloadTex():
-    response = await fetch(texURL)
+    response = await fetch(texURL,{},headers=headers)
     download_file(response, "LaTeX.dir")
 
 @when('click', '#downloadPDF')
