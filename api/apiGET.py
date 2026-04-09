7cf7f99f0c71480bfac501c4068678c11dd5c4ca
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-09 16:41:13 +0100

    Update apiGET.py

--- api/apiGET.py
+++ api/apiGET.py
@@ -14,3 +14,4 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         self.end_headers()
+    print(github_token)
