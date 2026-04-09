4bd2e18c1ca43a58bbd277a2ba96e3ceda96edc1
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-09 21:47:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 from pyodide.ffi import to_js
 import json
 import asyncio
-import pygit2
+import gzip
 
 texURL = 'https://github.com/Verillix/The-Greatest-Endeavour/tree/c48eee33166b79868bd92c364868b6d64cfb0019/LaTeX'
 pdfURL = 'https://raw.githubusercontent.com/Verillix/The-Greatest-Endeavour/refs/heads/main/The%20Greatest%20Endeavour.pdf'
@@ -21,7 +21,7 @@ headers = {
         'Access-Control-Allow-Headers' : 'Content-Type, Authorization'
 }
 
-def download_file(data, filename):
+def downloadTex(data, filename):
     #blob = Blob.new([data], {"type": "application/dir"})
     #url = URL.createObjectURL(blob)
     
@@ -33,28 +33,12 @@ def download_file(data, filename):
     document.body.removeChild(a)
     URL.revokeObjectURL(url)
 
-
-async def IterateDirectory(repo, branch):
-    tree = repo.revparse_single(branch).tree
-    trees_and_paths = [(tree, [])]
-    # keep going until there is no more data
-    while len(trees_and_paths) != 0:
-        tree, path = trees_and_paths.pop() # take the last entry
-        for entry in tree:
-            if entry.filemode == pygit2.GIT_FILEMODE_TREE:
-                next_tree = repo.get(entry.id)
-                next_path = list(path)
-                next_path.append(entry.name)
-                trees_and_paths.append((next_tree, next_path,))
-            else:
-                yield os.path.join(*path, entry.name)
-
-
 @when('change', '#upload')
 async def processTex(*args):
     content = document.getElementById('upload').files.item(0)
     content = await content.text()
-    filename = document.getElementById('upload').files.item(0).name 
+    filename = document.getElementById('upload').files.item(0).name
+    print(filename)    
 
 @when('click','#uploadToGit')
 async def push_file(content, filename):
@@ -69,8 +53,7 @@ async def push_file(content, filename):
 
 @when('click', '#downloadTex')
 async def downloadTex()
-    print(list(IterateDirectory("The-Greatest-Endeavour","main")))
-    download_file(texURL, "LaTeX.dir")
+    downloadTex(texURL, "LaTeX.dir")
 
 @when('click', '#downloadPDF')
 async def downloadPDF():
