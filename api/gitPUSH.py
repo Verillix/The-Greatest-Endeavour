35764ae6a5325af0867b63d5a23692a32657a04f
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-09 17:37:07 +0100

    Update gitPUSH.py

--- api/gitPUSH.py
+++ api/gitPUSH.py
@@ -3,6 +3,10 @@ import json, os
 from github import Github
 
 class handler(BaseHTTPRequestHandler):
+
+    g = Github("your_token_here")
+    repo = g.get_repo("owner/repo-name")
+    print(repo.full_name)
     def do_POST(self):
         length = int(self.headers.get('Content-Length', 0))
         body = json.loads(self.rfile.read(length))
