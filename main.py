a6a469d447261fdf38f26b6613f4f2a6b25f739d
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-10 00:21:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def push_file(content, filename):
 @when('click', '#downloadTex')
 async def downloadTex():
         response = await pyfetch("//api.github.com/repos/Verillix/The-Greatest-Endeavour/contents/LaTeX?ref=c48eee33166b79868bd92c364868b6d64cfb0019")
-        selector = Selector(text=response.text())
+        selector = Selector(text=response.text)
         tex_urls = selector.css('a[href$=".tex"]::attr(href)').getall()
         for i in tex_urls:
             tex_urls.remove(i)
