b859757046740dbec23b2b6bd1ecc01592d9337a
Verillix <139095640+Verillix@users.noreply.github.com>
2026-04-10 16:23:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,9 +53,10 @@ async def push_file(content, filename):
 @when('click', '#downloadTex')
 async def downloadTex():
         response = await pyfetch("//api.github.com/repos/Verillix/The-Greatest-Endeavour/contents/LaTeX?ref=c48eee33166b79868bd92c364868b6d64cfb0019")
-        text = json.parse(await response.json())
+        text = json.dumps(await response.json())
+        urlDict = json.loads(text)
         print("PLEASE WORK")
-        print(text.name)
+        print(urlDict['name'])
         #data = json.loads(text)
         #print(data)
         #download_Tex_File(content,data.get("name"))
